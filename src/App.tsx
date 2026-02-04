fe33f754fa0072b702075e2aedd40d011ef37d63
Teddy Leno <lenoteddy@gmail.com>
2026-02-04 12:26:15 +0700

    feat: update ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Logo from "./assets/logo.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="container">
+			<header>
+				<div className="py-4 flex items-center justify-between gap-x-6">
+					<div>
+						<a href="#" className="flex items-center">
+							<img src={Logo} alt="logo" className="w-28" />
+							<div className="ml-1">
+								<h1 className="text-xl leading-5 font-semibold">Omni Deposit</h1>
+								<p className="text-xs leading-5 font-light">Seamless Cross-Chain Asset Deposits</p>
+							</div>
+						</a>
+					</div>
+					<div>
+						<button className="border-2 py-2 px-4 rounded-xl text-sm font-semibold bg-black text-white cursor-pointer">Connect Wallet</button>
+					</div>
+				</div>
+			</header>
+			<main></main>
+			<footer></footer>
+		</div>
+	);
 }
 
-export default App
+export default App;
