d8ec72bbf616573ff3a3116d2a242f521b87fd2d
Teddy Leno <lenoteddy@gmail.com>
2026-02-04 22:39:40 +0700

    feat: ui menu for transaction confirmation

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import { useState } from "react";
 import { ConnectKitButton } from "connectkit";
 import StringHelper from "./helpers/StringHelper";
 import { chainList } from "./helpers/Web3Config";
-import SelectInput from "./components/SelectInput";
+import SelectInput, { type Option } from "./components/SelectInput";
 import AmountInput from "./components/AmountInput";
 import data from "./constants/chain-data.json";
 import Logo from "./assets/logo.png";
 
 function App() {
-	const [sourceChain, setSourceChain] = useState<string | null>(null);
-	const [sourceToken, setSourceToken] = useState<string | null>(null);
-	const [destinationChain, setDestinationChain] = useState<string | null>(null);
-	const [destinationToken, setDestinationToken] = useState<string | null>(null);
+	const [menu, setMenu] = useState<string>("CONFIRM");
+	const [sourceChain, setSourceChain] = useState<Option | null>(null);
+	const [sourceToken, setSourceToken] = useState<Option | null>(null);
+	const [destinationChain, setDestinationChain] = useState<Option | null>(null);
+	const [destinationToken, setDestinationToken] = useState<Option | null>(null);
 	const [amounts, setAmounts] = useState({ source: "0", destination: "0" });
 	const [protocol, setProtocol] = useState<string>("aave");
 	const protocolList = () => {
@@ -26,7 +27,7 @@ function App() {
 		return list;
 	};
 	const sourceTokenList = () => {
-		const list = sourceChain ? data[sourceChain as keyof typeof data].tokens : [];
+		const list = sourceChain ? data[sourceChain.value as keyof typeof data].tokens : [];
 		const newList = list.map((val) => ({
 			...val,
 			icon: <img src={val.icon} alt="" />,
@@ -34,7 +35,7 @@ function App() {
 		return newList;
 	};
 	const destinationTokenList = () => {
-		const list = destinationChain ? data[destinationChain as keyof typeof data].aave.tokens : [];
+		const list = destinationChain ? data[destinationChain.value as keyof typeof data].aave.tokens : [];
 		const newList = list.map((val) => ({
 			...val,
 			icon: <img src={val.icon} alt="" />,
@@ -81,11 +82,11 @@ function App() {
 						<div className="mt-4">
 							<div className="mb-2 relative z-20">
 								<label className="font-semibold">Source network</label>
-								<SelectInput placeholder="Choose a network" options={chainList} value={sourceChain} onChange={(option) => setSourceChain(option.value)} />
+								<SelectInput placeholder="Choose a network" options={chainList} value={sourceChain?.value} onChange={(option) => setSourceChain(option)} />
 							</div>
 							<div className="mb-2 relative z-10">
 								<label className="font-semibold">Source token</label>
-								<SelectInput placeholder="Choose a token" options={sourceTokenList()} value={sourceToken} onChange={(option) => setSourceToken(option.value)} />
+								<SelectInput placeholder="Choose a token" options={sourceTokenList()} value={sourceToken?.value} onChange={(option) => setSourceToken(option)} />
 							</div>
 							<div className="mb-2">
 								<label className="font-semibold">You send</label>
@@ -99,7 +100,7 @@ function App() {
 						<div className="mt-4">
 							<div className="mb-2 relative z-20">
 								<label className="font-semibold">Destination network</label>
-								<SelectInput placeholder="Choose a network" options={chainList} value={destinationChain} onChange={(option) => setDestinationChain(option.value)} />
+								<SelectInput placeholder="Choose a network" options={chainList} value={destinationChain?.value} onChange={(option) => setDestinationChain(option)} />
 							</div>
 							<div className="mb-2 flex items-center gap-x-2">
 								<div className="relative z-10 w-60">
@@ -108,7 +109,7 @@ function App() {
 								</div>
 								<div className="relative z-10 w-full">
 									<label className="font-semibold">Destination token</label>
-									<SelectInput placeholder="Choose a token" options={destinationTokenList()} value={destinationToken} onChange={(option) => setDestinationToken(option.value)} />
+									<SelectInput placeholder="Choose a token" options={destinationTokenList()} value={destinationToken?.value} onChange={(option) => setDestinationToken(option)} />
 								</div>
 							</div>
 							<div className="mb-2">
@@ -117,6 +118,82 @@ function App() {
 								<p className="text-sm italic">*enter either amount. The other will be calculated automatically.</p>
 							</div>
 						</div>
+					</div>
+				</div>
+				<div className="mt-4">
+					<div className="-mb-px">
+						<button
+							className={
+								"-mr-[0.5px] py-2 px-4 border border-black rounded-xl rounded-b-none rounded-r-none text-sm font-semibold cursor-pointer " +
+								(menu === "CONFIRM" ? "bg-black text-white" : "bg-white text-black")
+							}
+							onClick={() => setMenu("CONFIRM")}
+						>
+							CONFIRM
+						</button>
+						<button
+							className={
+								"-ml-[0.5px] py-2 px-4 border border-black rounded-xl rounded-b-none rounded-l-none text-sm font-semibold cursor-pointer " +
+								(menu === "HISTORY" ? "bg-black text-white" : "bg-white text-black")
+							}
+							onClick={() => setMenu("HISTORY")}
+						>
+							HISTORY
+						</button>
+					</div>
+					<div className="border rounded-tl-none rounded-lg p-4 shadow-[.3rem_.3rem_0_0_#000000]">
+						{menu === "CONFIRM" && (
+							<div>
+								<h3 className="mb-2 text-xl font-semibold">Confirm Transaction</h3>
+								{!sourceChain || !destinationChain || !sourceToken || !destinationToken || (Number(amounts.source) <= 0 && Number(amounts.destination) <= 0) ? (
+									<div className="my-4 text-center font-semibold">Please fill in the input on source chain and destination chain!</div>
+								) : (
+									<>
+										<p className="leading-8">
+											You are about to bridge
+											<span className="mx-1 px-1 border rounded-lg inline-flex items-center align-middle">
+												<div className="w-6 h-6">{sourceToken?.icon}</div>
+												{Number(amounts?.source) > 0 && <div className="mx-1 font-bold">{amounts?.source}</div>}
+												<div className="ml-1">{sourceToken?.label}</div>
+											</span>
+											from
+											<span className="mx-1 px-1 border rounded-lg inline-flex items-center align-middle">
+												<div className="w-6 h-6">{sourceChain?.icon}</div>
+												<div className="ml-1">{sourceChain?.label}</div>
+											</span>
+											to
+											<span className="mx-1 px-1 border rounded-lg inline-flex items-center align-middle">
+												<div className="w-6 h-6">{destinationChain?.icon}</div>
+												<div className="ml-1">{destinationChain?.label}</div>
+											</span>
+											and deposit it into
+											<span className="mx-1 px-1 border rounded-lg inline-flex items-center align-middle">
+												<div className="w-6 h-6">
+													<img src="https://cryptologos.cc/logos/aave-aave-logo.png" />
+												</div>
+												<div className="ml-1">Aave</div>
+											</span>
+											and receive
+											{Number(amounts?.destination) > 0 && <span className="px-1">(estimated)</span>}
+											<span className="mx-1 px-1 border rounded-lg inline-flex items-center align-middle">
+												<div className="w-6 h-6">{destinationToken?.icon}</div>
+												{Number(amounts?.destination) > 0 && <div className="mx-1 font-bold">{amounts?.destination}</div>}
+												<div className="ml-1">{destinationToken?.label}</div>
+											</span>
+											in return.
+										</p>
+										<div className="mt-2 text-center">
+											<button className="min-w-50 border-2 py-2 px-4 rounded-xl font-semibold bg-black text-white cursor-pointer">Submit Transaction</button>
+										</div>
+									</>
+								)}
+							</div>
+						)}
+						{menu === "HISTORY" && (
+							<div>
+								<div></div>
+							</div>
+						)}
 					</div>
 				</div>
 			</main>
